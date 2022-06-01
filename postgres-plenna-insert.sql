028e3a749ceff716fb9eaf15d44ebc5f788874b0
alearredondo <91506416+alearredondo@users.noreply.github.com>
2022-06-01 16:59:16 -0500

    Create postgres-plenna-insert.sql

---
+++ postgres-plenna-insert.sql
@@ -0,0 +1,65 @@
+--PLENNA SCRIPT inserts
+--BASE DE DATOS SCRIPT 2 
+
+
+--TABLA DOCTOR 
+
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Carolina', 'Zambrano', 'Medico General', 31, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Karla', 'Sanchez', 'Nutriologa', 40, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Hilda', 'Torres', 'Sexologa', 31, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Antonia', 'Rodriguez', 'Medico General', 34, 'Transgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Maricela', 'Lopez', 'Psicologa', 32, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Sonia', 'Cedeño', 'Medico General', 38, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Anna', 'Zabala', 'Nutriologa', 36, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Berenice', 'Jimenez', 'Ginecologa', 48, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Yadira', 'Garcia', 'Nutriologa', 37, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Mayra', 'Romero', 'Ginecologa', 42, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Judith', 'Castillo', 'Psicologa', 52, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Marisol', 'Gonzalez', 'Ginecologa', 32, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Rosario', 'Vera', 'Medico general', 51, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Reyna', 'Flores', 'Medico general', 42, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Aurora', 'Espinoza', 'Ginecologa', 39, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Olivia', 'Castro', 'Psicologa', 32, 'No Binario', '');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Raquel', 'Morales', 'Medico general', 33, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Irene', 'Perez', 'Sexologa', 32, 'Transgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Elvira', 'Bermudez', 'Psicologa', 30, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Catalina', 'Leon', 'Medico general', 52, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Lidia', 'Jaramillo', 'Ginecologa', 42, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Victoria', 'Mendoza', 'Sexologa', 27, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Dulce', 'Villavicencio', 'Medico general', 40, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Isela', 'Gonzalez', 'Psicologa', 38, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Sara', 'Rodriguez', 'Medico general', 37, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Marcela', 'Perez', 'Nutriologa', 44, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Elvia', 'Hernandez', 'Ginecologa', 36, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Esperanza', 'Garcia', 'Nutriologa', 58, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo)  values ('Cecilia', 'Martinez', 'Ginecologa', 36, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Dora', 'Sanchez', 'Psicologa', 32, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Eva', 'Lopez', 'Ginecologa', 58, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Julia', 'Diaz', 'Medico general', 55, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Elsa', 'Ferreira', 'Medico general', 45, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Esmeralda', 'Ramirez', 'Ginecologa', 55, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Brenda', 'Castillo', 'Psicologa', 32, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Socorro', 'Gomez', 'Medico general', 40, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Georgina', 'Romero', 'Sexologa', 35, 'Transgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Mercedes', 'Oliveira', 'Psicologa', 30, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Noemi', 'Mendoza', 'Nutriologa', 38, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Celia', 'Rossi', 'Sexologa', 53, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Daniela', 'Silva', 'Medico general', 41, 'Transgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Ivanna', 'Gutierrez', 'Psicologa', 34, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Minerva', 'Rodriguez', 'Medico general', 43, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Nora', 'Gonzalez', 'Nutriologa', 55, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Griselda', 'Martinez', 'Ginecologa', 46, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Elia', 'Fernandez', 'Nutriologa', 37, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Rosalba', 'Perez', 'Ginecologa', 43, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Imelda', 'Garcia', 'Psicologa', 40, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Ivonne', 'Silva', 'Ginecologa', 39, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Marina', 'Lopez', 'Medico general', 44, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Alejandra', 'Pereira', 'Medico general', 50, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Lizbeth', 'Sosa', 'Ginecologa', 42, 'Transgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Mireya', 'Galvan', 'Psicologa', 60, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Soledad', 'Garcia', 'Medico general', 52, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Clara', 'Zavala', 'Sexologa', 49, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Lourdes', 'Gasamans', 'Psicologa', 46, 'Transgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Ines', 'Rivas', 'Medico general', 62, 'Cisgenero', 'Femenino');
+insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Aida', 'Beltran', 'Psicologa', 57, 'Transgenero', 'Femenino');
+
