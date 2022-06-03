512c7585842d7fc61c14baf2e92e67a5e933a3eb
alearredondo <91506416+alearredondo@users.noreply.github.com>
2022-06-02 19:02:47 -0500

    paciente

--- postgres-plenna-insert.sql
+++ postgres-plenna-insert.sql
@@ -1,9 +1,9 @@
 --PLENNA SCRIPT inserts
 --BASE DE DATOS SCRIPT 2 
 
-
+--
 --TABLA DOCTOR 
-
+--
 insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Carolina', 'Zambrano', 'Medico General', 31, 'Cisgenero', 'Femenino');
 insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Karla', 'Sanchez', 'Nutriologa', 40, 'Cisgenero', 'Femenino');
 insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Hilda', 'Torres', 'Sexologa', 31, 'Cisgenero', 'Femenino');
@@ -62,4 +62,131 @@ insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values 
 insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Lourdes', 'Gasamans', 'Psicologa', 46, 'Transgenero', 'Femenino');
 insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Ines', 'Rivas', 'Medico general', 62, 'Cisgenero', 'Femenino');
 insert into doctor (nombre, apellidos, especialidad, edad, genero, sexo) values ('Aida', 'Beltran', 'Psicologa', 57, 'Transgenero', 'Femenino');
+--
+--
+--TABLA PACIENTE
+--
+--
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Maria', 'Gonzalez', 18, 'Cisgenero', 'Femenino', 'Nutriologa',  'B+', 45.02, 150.14, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Guadalupe', 'Muñoz', 54, 'Cisgenero', 'Femenino', 'Sexologa', 'O-', 66.07, 167.34, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Rosa', 'Rojas', 15, 'Cisgenero', 'Femenino', 'Psicologa y Nutriologa', 'O+', 60.70, 155.15, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Martha', 'Diaz', 36, 'Cisgenero', 'Femenino', 'Nutriologa', 'A+', 50.33, 170.03, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Ana', 'Perez', 29, 'Transgenero', 'Femenino','Medico general', 'O+', 61.10, 165.14, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Patricia', 'Soto', 26, 'Cisgenero', 'Femenino','Ginecologa, Psicologa', 'O+', 77.02, 149.70, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Leticia', 'Contreras', 23, 'Cisgenero', 'Femenino', 'Sexologa, Psicologa', 'B-', 54.80, 154.60, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Elena', 'Silva', 45, 'Cisgenero', 'Femenino', 'Psicologa', 'A+', 80.90, 163.14, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Elizabeth', 'Zepulveda', 19, 'Cisgenero', 'Femenino', 'Nutriologa', 'B+', 50.80, 153.00, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Isabel', 'Morales', 60, 'Cisgenero', 'Femenino', 'Ginecologa', 'O+', 88.80, 175.80, 'Retirada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Alicia', 'Rodriguez', 40, 'Cisgenero', 'Femenino', 'Sexologa', 'AB+', 67.80, 176.89, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Margarita', 'Lopez', 38, 'Cisgenero', 'Femenino', 'Psicologa, Sexologa', 'O+', 76.60, 182.89, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Luz', 'Fuentes', 26, 'Cisgenero', 'Femenino', 'Nutriologa, Psicologa, Medico general','O+', 80.80, 166.10, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Angelica', 'Hernandez', 29, 'Cisgenero', 'Femenino', 'Medico general', 'AB+', 51.10, 157.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Claudia', 'Torres', 37, 'Transgenero', 'Femenino', 'Ginecologa','O-', 67.80, 165.99, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Silvia', 'Araya', 19, 'No Binario', ' ', 'Sexologa' ,'O+', 52.80, 147.90, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Norma', 'Flores', 41, 'Cisgenero', 'Femenino', 'Psicologa, Nutriologa' ,'B+', 69.70, 164.90, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Juana', 'Espinoza', 27, 'Cisgenero', 'Femenino', 'Nutriologa','O+', 90.83, 165.60, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Gabriela', 'Valenzuela', 21, 'Cisgenero', 'Femenino', 'Medico general' ,'O+', 77.10, 154.89, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Adriana', 'Castillo', 38, 'Cisgenero', 'Femenino', 'Ginecologa' ,'A+', 57.90, 164.19, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values 
+('Veronica', 'Tapia', 18, 'Transgenero', 'Femenino', 'Sexologa, Nutriologa' ,'B+', 53.80, 153.89, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Teresa', 'Reyes', 25, 'Cisgenero', 'Femenino', 'Psicologa' ,'AB+', 65.80, 168.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Alejandra', 'Gutierrez', 42, 'Cisgenero', 'Femenino', 'Nutriologa' ,'B+', 55.00, 161.10, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Alma', 'Castro', 37, 'Cisgenero', 'Femenino', 'Medico general' ,'AB-', 65.78, 154.89, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Beatriz', 'Pizarro', 22, 'Cisgenero', 'Femenino', 'Ginecologa, Sexologa' ,'O+', 62.10, 164.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Martha', 'Alvarez', 18, 'Transgenero', 'Femenino', 'Sexologa' ,'O+', 61.10, 161.99, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Yolanda', 'Vazquez', 25, 'Cisgenero', 'Femenino', 'Psicologa' ,'O-', 87.10, 160.09, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Blanca', 'Sanchez', 32, 'Cisgenero', 'Femenino', 'Nutriologa' ,'A+', 62.00, 151.80, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Sandra', 'Fernandez', 24, 'Cisgenero', 'Femenino', 'Medico general, Psicologa' ,'O+', 73.80, 170.89, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Araceli', 'Ramirez', 27, 'Cisgenero', 'Femenino', 'Ginecologa, Sexologa' ,'O-', 55.80, 163.20, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Irma', 'Carrasco', 35, 'Cisgenero', 'Femenino', 'Sexologa' ,'O+', 56.20, 161.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Gloria', 'Gomez', 56, 'Cisgenero', 'Femenino', 'Sexologa' ,'O+', 60.90, 156.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Carmen', 'Cortes', 60, 'Cisgenero', 'Femenino','Nutriologa, Sexologa, Psicologa, Medico general' ,'O+', 77.10, 170.09, 'Retirada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Luisa', 'Herrera', 20, 'Transgenero', 'Femenino', 'AB-', 52.10, 163.99, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Rocio', 'Nuñez', 31, 'Cisgenero', 'Femenino','Ginecologa, Sexologa' ,'O+', 66.80, 165.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Concepcion', 'Jara', 32, 'Cisgenero', 'Femenino','Sexologa' ,'O+', 61.00, 160.09, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Cristina', 'Vergara', 29, 'Cisgenero', 'Femenino', 'Psicologa, Nutriologa' ,'O+', 54.90, 162.79, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Edith', 'Rivera', 28, 'Transgenero', 'Femenino', 'Nutriologa' ,'B+', 59.00, 160.79, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Esther', 'Figueroa', 30, 'Cisgenero', 'Femenino', 'Medico general' ,'O+', 64.10, 158.99, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Lilia', 'Riquelme', 34, 'Cisgenero', 'Femenino', 'Ginecologa' ,'A+', 50.90, 167.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Lorena', 'Garcia', 20, 'Cisgenero', 'Femenino', 'Sexologa' ,'AB+', 68.10, 177.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Magdalena', 'Miranda', 41, 'Cisgenero', 'Femenino', 'Psicologa, Medico general' ,'O+', 69.00, 168.10, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Cecilia', 'Bravo', 30, 'Cisgenero', 'Femenino', 'Nutriologa' ,'B+', 57.00, 173.09, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Josefina', 'Vera', 21, 'Cisgenero', 'Femenino', 'Medico general' ,'A+', 75.00, 167.54, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Dolores', 'Molina', 26, 'Cisgenero', 'Femenino', 'Ginecologa' ,'A+', 62.60, 156.45, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Karina', 'Vega', 28, 'Cisgenero', 'Femenino', 'Sexologa' ,'O+', 58.10, 167.90, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Lucia', 'Campos', 19, 'Cisgenero', 'Femenino', 'Psicologa, Medico general' ,'A+', 71.00, 165.90, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Estela', 'Sandoval', 20, 'Cisgenero', 'Femenino', 'Nutriologa' ,'O+', 88.00, 150.79, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Susana', 'Orellana', 34, 'Cisgenero', 'Femenino', 'Medico general, Psicologa' ,'B-', 58.90, 167.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Maribel', 'Cardenas', 30, 'Cisgenero', 'Femenino', 'Ginecologa' ,'O-', 48.00, 151.99, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Anna', 'Olivares', 24, 'Cisgenero', 'Femenino', 'Nutriologa, Sexologa, Psicologa, Medico general' ,'O+', 53.67, 164.80, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Lourdes', 'Alarcon', 21, 'Cisgenero', 'Femenino', 'Psicologa, Nutriologa' ,'O+', 61.00, 163.21, 'Estudiante');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Erika', 'Gallardo', 23, 'Cisgenero', 'Femenino', 'Nutriologa' ,'B+', 52.00, 176.00, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Monica', 'Ortiz', 26, 'Cisgenero', 'Femenino', 'Medico general' ,'AB-', 60.10, 168.99, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Laura', 'Garrido', 45, 'Cisgenero', 'Femenino', 'Ginecologa' ,'O+', 57.00, 159.65, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Diana', 'Salazar', 36, 'Transgenero', 'Femenino', 'Psicologa, Sexologa' ,'B+', 64.89, 175.90, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Eugenia', 'Guzman', 30, 'No binario', ' ', 'Psicologa' ,'O+', 64.89, 156.89, 'Empleada');
+insert into paciente (nombre, apellidos, edad, genero, sexo, doctora_visita, tipo_sangre, peso, estatura, ocupacion) values
+('Cecilia', 'Saavedra', 22, 'Cisgenero', 'Femenino', 'Psicologa, Nutriologa' ,'O+', 60.90, 154.00, 'Estudiante');
+
+
+
+
+
+
 
