0a946b67c727c527a25c776fdb17d562135ebf91
alearredondo <91506416+alearredondo@users.noreply.github.com>
2022-06-02 19:05:52 -0500

    Create postgres-plenna-drop-objects.sql

---
+++ postgres-plenna-drop-objects.sql
@@ -0,0 +1,31 @@
+-- DROP TABLES
+
+DROP TABLE doctor CASCADE;
+DROP TABLE paciente CASCADE;
+DROP TABLE asistente CASCADE;
+DROP TABLE especialidad CASCADE;
+DROP TABLE doctor_especialidad CASCADE;
+DROP TABLE tipo_consulta CASCADE;
+DROP TABLE consulta CASCADE;
+DROP TABLE encuesta CASCADE;
+DROP TABLE expediente CASCADE;
+DROP TABLE tratamiento CASCADE;
+DROP TABLE notas CASCADE;
+DROP TABLE seguimiento CASCADE;
+
+-- DROP SEQUENCES
+
+DROP SEQUENCE doctor_id_doctor_seq;
+DROP SEQUENCE paciente_id_paciente_seq;
+DROP SEQUENCE asistente_id_asistente_seq;
+DROP SEQUENCE especialidad_id_especialidad_seq;
+DROP SEQUENCE tipo_consulta_id_tipo_consulta_seq;
+DROP SEQUENCE consulta_id_consulta_seq;
+DROP SEQUENCE encuesta_id_encuesta_seq;
+DROP SEQUENCE expediente_id_expediente_seq;
+DROP SEQUENCE tratamiento_id_tratamiento_seq;
+DROP SEQUENCE notas_id_notas_seq;
+DROP SEQUENCE seguimiento_id_seguimiento_seq;
+
+
+
