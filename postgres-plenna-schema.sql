d8eeb976e8329c67c4d8439281e8bc84b219111b
alearredondo <91506416+alearredondo@users.noreply.github.com>
2022-06-05 16:48:59 -0500

    Update postgres-plenna-schema.sql

--- postgres-plenna-schema.sql
+++ postgres-plenna-schema.sql
@@ -1,39 +1,99 @@
 --PLENNA SCRIPT TABLAS 
 --BASE DE DATOS SCRIPT 1
 
+create type identidad_genero as enum (
+    'mujer',
+    'hombre',
+    'no binario'
+);
+create type sexo_biologico as enum (
+    'femenino',
+    'masculino',
+    'intersexual'
+);
+create type enfermedad as enum (
+    'hipertension',
+    'diabetes',
+    'hipotiroidismo',
+    'sindrome de ovario poliquistico',
+    'anemia',
+    'ninguna',
+    'otro'
+);
+create type fecha_menstruacion as enum (
+    'antes de los 10',
+    'de los 10 a los 14',
+    'despues de los 14'
+);
+create type metodo as enum (
+    'condon',
+    'sistema de larga duracion hormonal',
+    'diu',
+    'implante subdermico',
+    'anticonceptivos orales combinados',
+    'anillo vaginal',
+    'parche',
+    'inyeccion',
+    'ciclo natural',
+    'espermicidas',
+    'coitus interruptus',
+    'ligacion o corte de trompas uterinas',
+    'vasectomia',
+    'ninguno',
+    'otro'
+);
+create type ejercicio as enum (
+    'caminar',
+    'eliptica',
+    'bicicleta',
+    'pesas',
+    'yoga',
+    'actividades al aire libre',
+    'ninguno',
+    'otro'
+);
+create type frecuencia as enum (
+    'nunca',
+    'pocas veces',
+    'a veces',
+    'frecuentemente',
+    'siempre'
+);
+create type desde as enum (
+    'menos de 2 semanas',
+    '2 semanas',
+    'mas de 2 semanas'
+);
+--
+
+-- Tablas
 --
 -- TABLE: doctor
 --  
-
 create table doctor (
-  id_doctor integer (7) constraint pk_doctor primary key,
-  nombre varchar(50) NOT NULL,
-  apellidos varchar(50) NOT NULL,
-  especialidad varchar (50) not null,
-  edad numeric(3,0) NOT NULL,
-  genero varchar(50) not NULL,
-  sexo varchar(50) NOT NULL
+  id_doctor numeric(7,0) constraint pk_doctor primary key,
+  nombre varchar(50) not null,
+  apellidos varchar(50) not null,
+  edad numeric(3,0) not null,
+  genero identidad_genero default 'mujer'::identidad_genero not null,
+  sexo sexo_biologico default 'femenino'::sexo_biologico not null
 );
 --
-CREATE SEQUENCE doctor_id_doctor_seq START 1000000 INCREMENT 1 ;
-ALTER TABLE doctor ALTER COLUMN id_doctor SET DEFAULT nextval('doctor_id_doctor_seq');
+create sequence doctor_id_doctor_seq start 1000000 increment 1 ;
+alter table doctor alter column id_doctor set default nextval('doctor_id_doctor_seq');
 --
-
 
 --
 -- TABLE: paciente
 -- 
 create TABLE paciente (
-  id_paciente integer(7) constraint pk_paciente primary key,
+  id_paciente numeric(7,0) constraint pk_paciente primary key,
   nombre varchar(50) NOT NULL,
   apellidos varchar(50) NOT NULL,
-  edad integer(3) NOT null,
-  genero varchar(50) NOT null,
-  sexo varchar(50) NOT null,
-  doctora_visita varchar (100) not null,
+  edad numeric(3,0) NOT null,
+  genero identidad_genero default 'mujer'::identidad_genero not null,
+  sexo sexo_biologico default 'femenino'::sexo_biologico not null,
   tipo_sangre char(2) NOT NULL,
-  peso numeric(5,2) not null,
-  estatura numeric(5,2) not null,
   ocupacion varchar(50) NOT NULL
 );
 create SEQUENCE paciente_id_paciente_seq START 2000000 INCREMENT 1 ;
@@ -53,11 +113,22 @@ ALTER table asistente ALTER COLUMN id_asistente SET DEFAULT nextval('asistente_i
 --
 
 --
+-- TABLE: especialidad
+-- 
+CREATE TABLE especialidad (
+  id_especialidad numeric(7,0) constraint pk_especialidad primary key,
+  nombre varchar(50) not null 
+);
+CREATE SEQUENCE especialidad_id_especialidad_seq START 1 INCREMENT 1 ;
+ALTER TABLE especialidad ALTER COLUMN id_especialidad SET DEFAULT nextval('especialidad_id_especialidad_seq');
+--
+
+--
 -- TABLE: doctor_especialidad
 --  
 CREATE TABLE doctor_especialidad (
   id_doctor numeric(7,0) references doctor (id_doctor) ON UPDATE CASCADE ON DELETE CASCADE,
-  id_especialidad numeric(7,0) references especialidad (id_especialidad) ON UPDATE cascade,
+  id_especialidad numeric(7,0) references especialidad (id_especialidad) ON UPDATE cascade ON DELETE CASCADE,
   constraint pk_doctor_especialidad primary key (id_doctor, id_especialidad)
 );
 -- 
@@ -84,94 +155,12 @@ create TABLE consulta (
   id_paciente numeric(7,0) references paciente (id_paciente),
   fecha date not null,
   hora_inicio time NOT NULL,
-  hora_fin time not null
+  hora_fin time not null,
+  notas_doctor varchar(500) NOT NULL
 );
 CREATE SEQUENCE consulta_id_consulta_seq START 1 INCREMENT 1 ;
 ALTER TABLE consulta ALTER COLUMN id_consulta SET DEFAULT nextval('consulta_id_consulta_seq');
 -- 
-
---
--- TABLE: encuesta
---
-CREATE TABLE encuesta (
-    id_encuesta numeric(7,0) constraint pk_encuesta primary key,
-    pregunta1 varchar(100) NOT NULL,
-    pregunta2 varchar(100) NOT NULL,
-    pregunta3 varchar(100) NOT NULL,
-    pregunta4 varchar(100) NOT NULL,
-    pregunta5 varchar(100) NOT NULL,
-    pregunta6 varchar(100) NOT NULL,
-    pregunta7 varchar(100) NOT NULL,
-    pregunta8 varchar(100) NOT NULL,
-    pregunta9 varchar(100) NOT NULL,
-    pregunta10 varchar(100) NOT NULL,
-    pregunta11 varchar(100) NOT NULL,
-    pregunta12 varchar(100) NOT NULL,
-    pregunta13 varchar(100) NOT NULL,
-    pregunta14 varchar(100) NOT NULL,
-    pregunta15 varchar(100) NOT NULL,
-    pregunta16 varchar(100) NOT NULL,
-    pregunta17 varchar(100) NOT NULL,
-    pregunta18 varchar(100) NOT NULL,
-    pregunta19 varchar(100) NOT NULL,
-    pregunta20 varchar(100) NOT NULL,
-    pregunta21 varchar(100) NOT NULL,
-    pregunta22 varchar(100) NOT NULL,
-    pregunta23 varchar(100) NOT NULL,
-    pregunta24 varchar(100) NOT NULL,
-    pregunta25 varchar(100) NOT NULL,
-    pregunta26 varchar(100) NOT NULL,
-    pregunta27 varchar(100) NOT NULL,
-    pregunta28 varchar(100) NOT NULL,
-    pregunta29 varchar(100) NOT NULL,
-    pregunta30 varchar(100) NOT NULL
-);
-CREATE SEQUENCE encuesta_id_encuesta_seq START 1 INCREMENT 1 ;
-ALTER TABLE encuesta ALTER COLUMN id_encuesta SET DEFAULT nextval('encuesta_id_encuesta_seq');
---
-
---
--- TABLE: expediente
---
-CREATE TABLE expediente (
-    id_expediente numeric(7,0) constraint pk_expediente primary key,
-    id_paciente numeric(7,0) references paciente (id_paciente),
-    id_doctor numeric(7,0) references doctor (id_doctor),
-    id_encuesta numeric(7,0) references encuesta (id_encuesta),
-    respuesta1 varchar(100) NOT NULL,
-    respuesta2 varchar(100) NOT NULL,
-    respuesta3 varchar(100) NOT NULL,
-    respuesta4 varchar(100) NOT NULL,
-    respuesta5 varchar(100) NOT NULL,
-    respuesta6 varchar(100) NOT NULL,
-    respuesta7 varchar(100) NOT NULL,
-    respuesta8 varchar(100) NOT NULL,
-    respuesta9 varchar(100) NOT NULL,
-    respuesta10 varchar(100) NOT NULL,
-    respuesta11 varchar(100) NOT NULL,
-    respuesta12 varchar(100) NOT NULL,
-    respuesta13 varchar(100) NOT NULL,
-    respuesta14 varchar(100) NOT NULL,
-    respuesta15 varchar(100) NOT NULL,
-    respuesta16 varchar(100) NOT NULL,
-    respuesta17 varchar(100) NOT NULL,
-    respuesta18 varchar(100) NOT NULL,
-    respuesta19 varchar(100) NOT NULL,
-    respuesta20 varchar(100) NOT NULL,
-    respuesta21 varchar(100) NOT NULL,
-    respuesta22 varchar(100) NOT NULL,
-    respuesta23 varchar(100) NOT NULL,
-    respuesta24 varchar(100) NOT NULL,
-    respuesta25 varchar(100) NOT NULL,
-    respuesta26 varchar(100) NOT NULL,
-    respuesta27 varchar(100) NOT NULL,
-    respuesta28 varchar(100) NOT NULL,
-    respuesta29 varchar(100) NOT NULL,
-    respuesta30 varchar(100) NOT NULL
-);
-CREATE SEQUENCE expediente_id_expediente_seq START 1 INCREMENT 1 ;
-ALTER TABLE expediente ALTER COLUMN id_expediente SET DEFAULT nextval('expediente_id_expediente_seq');
---
 
 --
 -- TABLE: tratamiento
@@ -185,45 +174,104 @@ create TABLE tratamiento (
   medicamento varchar(100) NOT NULL,
   marca varchar(100) NOT NULL,
   dosis varchar(100) NOT NULL,
-  instrucciones varchar(100) NOT NULL,
+  instrucciones varchar(500) NOT NULL,
   fecha_inicio timestamp NOT NULL,
-  fecha_fin timestamp not null
+  fecha_fin timestamp
 );
 CREATE SEQUENCE tratamiento_id_tratamiento_seq START 1 INCREMENT 1 ;
 ALTER TABLE tratamiento ALTER COLUMN id_tratamiento SET DEFAULT nextval('tratamiento_id_tratamiento_seq');
 -- 
 
 --
--- TABLE: notas
--- 
-create TABLE notas (
-  id_notas numeric(7,0) constraint pk_notas primary key,
-  id_consulta numeric(7,0) references consulta (id_consulta),
-  id_doctor numeric(7,0) references doctor (id_doctor),
-  id_paciente numeric(7,0) references paciente (id_paciente),
-  id_expediente numeric(7,0) references expediente (id_expediente),
-  id_tratamiento numeric(7,0) references tratamiento (id_tratamiento),
-  descripcion varchar(500) NOT NULL,
-  fecha_realizacion timestamp not null
-);
-CREATE SEQUENCE notas_id_notas_seq START 1 INCREMENT 1 ;
-ALTER TABLE notas ALTER COLUMN id_notas SET DEFAULT nextval('notas_id_notas_seq');
--- 
-
+-- TABLE: expediente
 --
--- TABLE: seguimiento
--- 
-create TABLE seguimiento (
-  id_seguimiento numeric(7,0) constraint pk_seguimiento primary key,
-  id_consulta numeric(7,0) references consulta (id_consulta),
-  id_expediente numeric(7,0) references expediente (id_expediente),
-  descripcion varchar(500) not null
+CREATE TABLE expediente (
+    id_expediente numeric(7,0) constraint pk_expediente primary key,
+    id_paciente numeric(7,0) references paciente (id_paciente),
+    que_enfermedad enfermedad default 'ninguna'::enfermedad not null,
+    toma_medicamentos boolean default false NOT NULL,
+    medicamentos varchar(500)
 );
-CREATE SEQUENCE seguimiento_id_seguimiento_seq START 1 INCREMENT 1 ;
-ALTER TABLE seguimiento ALTER COLUMN id_seguimiento SET DEFAULT nextval('seguimiento_id_seguimiento_seq');
+CREATE SEQUENCE expediente_id_expediente_seq START 1 INCREMENT 1 ;
+ALTER TABLE expediente ALTER COLUMN id_expediente SET DEFAULT nextval('expediente_id_expediente_seq');
 --
 
+--
+-- TABLE: ginecologia
+--
+CREATE TABLE ginecologia (
+    id_ginecologia numeric(7,0) constraint pk_ginecologia primary key,
+    id_expediente numeric(7,0) references expediente (id_expediente),
+    primer_menstruacion fecha_menstruacion default 'antes de los 10'::fecha_menstruacion,
+    ultima_menstruacion date,
+    inicio_vida_sexual numeric(2,0),
+    metodo_anticonceptivo metodo default 'ninguno'::metodo not null
+);
+CREATE SEQUENCE ginecologia_id_ginecologia_seq START 1 INCREMENT 1 ;
+ALTER TABLE ginecologia ALTER COLUMN id_ginecologia SET DEFAULT nextval('ginecologia_id_ginecologia_seq');
+--
 
+--
+-- TABLE: nutricion
+--
+CREATE TABLE nutricion (
+    id_nutricion numeric(7,0) constraint pk_nutricion primary key,
+    id_expediente numeric(7,0) references expediente (id_expediente),
+    peso numeric(5,2) NOT NULL,
+    estatura numeric(3,2) NOT NULL,
+    litros_agua_diaria numeric(3,2) not null,
+    hace_ejercicio boolean default false NOT null,
+    dias_ejercicio numeric(1,0),
+    tipo_ejercicio ejercicio default 'ninguno'::ejercicio not null
+);
+CREATE SEQUENCE nutricion_id_nutricion_seq START 1 INCREMENT 1 ;
+ALTER TABLE nutricion ALTER COLUMN id_nutricion SET DEFAULT nextval('nutricion_id_nutricion_seq');
+--
+
+--
+-- TABLE: psicologia
+--
+CREATE TABLE psicologia (
+    id_psicologia numeric(7,0) constraint pk_psicologia primary key,
+    id_expediente numeric(7,0) references expediente (id_expediente),
+    preocupaciones varchar(500) NOT NULL,
+    desde_cuando desde default 'menos de 2 semanas'::desde not null,
+    agotado_emocionalmente frecuencia default 'siempre'::frecuencia not null,
+    vida_afectada boolean default true NOT null
+);
+CREATE SEQUENCE psicologia_id_psicologia_seq START 1 INCREMENT 1 ;
+ALTER TABLE psicologia ALTER COLUMN id_psicologia SET DEFAULT nextval('psicologia_id_psicologia_seq');
+--
+
+--
+-- TABLE: sexologia
+--
+CREATE TABLE sexologia (
+    id_sexologia numeric(7,0) constraint pk_sexologia primary key,
+    id_expediente numeric(7,0) references expediente (id_expediente),
+    dificultad_orgasmo boolean default true NOT null,
+    frecuencia_dificultad_orgasmo frecuencia default 'siempre'::frecuencia not null,
+    comformidad_vida_sexual boolean default false not null,
+    seguridad_sexualidad boolean default true NOT null
+);
+CREATE SEQUENCE sexologia_id_sexologia_seq START 1 INCREMENT 1 ;
+ALTER TABLE sexologia ALTER COLUMN id_sexologia SET DEFAULT nextval('sexologia_id_sexologia_seq');
+--
+
+--
+-- TABLE: suenio
+--
+CREATE TABLE suenio (
+    id_suenio numeric(7,0) constraint pk_suenio primary key,
+    id_expediente numeric(7,0) references expediente (id_expediente),
+    horas_suenio numeric(3,1) NOT NULL,
+    ronca boolean default true not null,
+    dificultad_suenio frecuencia default 'siempre'::frecuencia not null,
+    pesadillas boolean default true not null
+);
+CREATE SEQUENCE suenio_id_suenio_seq START 1 INCREMENT 1 ;
+ALTER TABLE suenio ALTER COLUMN id_suenio SET DEFAULT nextval('suenio_id_suenio_seq');
+--
 
 
 
