af9c3f792ed68295dc6b3978bf38b0879aab26c5
elisagzafra <98352553+elisagzafra@users.noreply.github.com>
2022-06-08 12:41:48 -0500

    Add files via upload

--- postgres-plenna-delete-data.sql
+++ postgres-plenna-delete-data.sql
@@ -1,16 +1,21 @@
 -- DELETE DATA
 
-DELETE FROM doctor;
-DELETE FROM paciente;
-DELETE FROM asistente;
-DELETE FROM especialidad;
-DELETE FROM doctor_especialidad;
-DELETE FROM tipo_consulta;
-DELETE FROM consulta;
-DELETE FROM tratamiento;
-DELETE FROM expediente;
-DELETE FROM ginecologia;
-DELETE FROM nutricion;
-DELETE FROM sexologia;
-DELETE FROM psicologia;
-DELETE FROM suenio;
+DELETE FROM doctor CASCADE;
+DELETE FROM paciente CASCADE;
+DELETE FROM asistente CASCADE;
+DELETE FROM especialidad CASCADE;
+DELETE FROM doctor_especialidad CASCADE;
+DELETE FROM tipo_consulta CASCADE;
+DELETE FROM consulta CASCADE;
+DELETE FROM tratamiento CASCADE;
+DELETE FROM expediente CASCADE;
+DELETE FROM ginecologia CASCADE;
+DELETE FROM nutricion CASCADE;
+DELETE FROM sexologia CASCADE;
+DELETE FROM psicologia CASCADE;
+DELETE FROM suenio CASCADE;
+
+
+
+
+
